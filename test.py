5accc5b82b8b03b9b3138a2075980ad360c42279
Luis Torres <luisfciencias@gmail.com>
2019-06-07 23:55:01 +1000

    NEW: Add datetime conversion

--- test.py
+++ test.py
@@ -2,9 +2,18 @@
 import pandas as pd
 
 # template to load time series
-column_names = ['utc', 'voltaje']
+column_names = ['utc_string', 'voltaje']
 df = pd.read_csv('2019-01-24_L1.txt', names=column_names,
                  header=None, delim_whitespace=True)
-print(df.shape)
+
+# single call for datetime conversion
+year = '2019'
+month = '01'
+day = '24'
+date = year + '-' + month + '-' + day
+df['utc_string'] = [date + ' ' + item for item in df['utc_string']]
+format_datetime_string = "%Y-%m-%d %H:%M:%S.%f"
+df['utc_datetime'] = pd.to_datetime(df['utc_string'],
+                                    format=format_datetime_string)
+print(df)
 print(df.dtypes)
-print(df.head())
